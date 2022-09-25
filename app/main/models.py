61e75984ff9408a6bdfb0e3fb2b6d1099243a921
BezrabotnyFreelancer <ilya.vaitehovich@mail.ru>
2022-09-25 15:18:00 +0300

    new files

--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+class TempGenPassword(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    password = models.CharField(max_length=30)
+    
+    
+    def delete_record(user):
+        try:
+            instance = TempGenPassword.objects.get(user=user)
+            instance.delete()
+        except:
+            return None
+        
+    def add_record(user, password):
+        passwd = TempGenPassword()
+        passwd.user = user
+        passwd.password = password
+        passwd.save()
--- app/main/views.py
+++ app/main/views.py
@@ -4,41 +4,39 @@ from .error import EmptyString
 from .generator import generate_password
 from storage.secure import encode_password, key
 from storage.models import PasswordStorage
+from .models import TempGenPassword
 from .template_render import index_html
 
 def return_password(request):
-    final_password = ''
-    if request.method == 'POST':
+    if request.method == 'POST' and request.POST.get('site') == None or request.POST.get('site') == '':
+               
         length = request.POST.get('length')
         
         try:
-            final_password += generate_password(int(length))
-            context = {'password': final_password, 'length': length}
-            
-            if 'site' in request.POST:
-                try:
-                    url = request.POST.get('site')
-                    if url == '':
-                        raise EmptyString
-                
-                except EmptyString:
-                    return index_html(request, context)
-                
-                encoded_password = encode_password(final_password)
-                generated_object = PasswordStorage
-                generated_object.create_passwd_from_generator(
-                    user=request.user,
-                    site=url,
-                    password=encoded_password,
-                    key=key                        
-                )
-                
-                return redirect('storage_list')
-            
-            return index_html(request, context)       
-            
+            final_password = generate_password(int(length))
+            context = {'password': final_password, 'length': length}          
         except:
-            return index_html(request)    
+            return index_html(request)
+        
+        if request.user.is_authenticated:
+            instance = TempGenPassword
+            instance.delete_record(request.user)
+            instance.add_record(request.user, final_password)
+        return index_html(request, context)
+    
+    elif request.method == 'POST' and request.POST.get('site') != '':
+        url = request.POST.get('site')
+        not_encrypted_password = TempGenPassword.objects.get(user=request.user)
+        encrypted_password = encode_password(not_encrypted_password.password)
+        instance = PasswordStorage
+        instance.create_passwd_from_generator(
+            user=request.user,
+            site=url,
+            password=encrypted_password,
+            key=key
+        )
+        return redirect('storage_list')
+    
     else:
         return index_html(request)
     
